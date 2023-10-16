c918806dc39e3ad000f3105ef01aa745b8861211
fabricnogueira <fabricio221@gmail.com>
2023-10-16 08:54:33 -0300

    Comment out CSS and JS calls and remove auths from routes

--- test/system/auths_test.rb
+++ test/system/auths_test.rb
@@ -3,8 +3,8 @@
 require 'application_system_test_case'
 
 class AuthsTest < ApplicationSystemTestCase
-  test 'visiting the index' do
-    visit root_path
-    assert_selector 'h1', text: 'Auth'
+  test "visiting the index" do
+    visit root_url
+    assert_selector "h1", text: "Minha Página Inicial"
   end
-end
+end  
