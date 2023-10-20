58d85feb09503751c02de7de502bc637e25cd858
fabricnogueira <fabricio221@gmail.com>
2023-10-20 14:36:19 -0300

    Add login routing, update authentication tests and change screen size in system tests

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@
 
 Rails.application.routes.draw do
   root 'home#index'
+
+  # Defina a rota para login como sessions#new
+  get 'login', to: 'login#new', as: 'login'
 end
--- test/system/auths_test.rb
+++ test/system/auths_test.rb
@@ -6,5 +6,7 @@ class AuthsTest < ApplicationSystemTestCase
   test "visiting the index" do
     visit root_url
     assert_selector "h1", text: "Minha Página Inicial"
+    click_on "Crie sua conta"
+    fill_in "Qual o seu email?", with: "basico179@hotmail.com"
   end
 end  
