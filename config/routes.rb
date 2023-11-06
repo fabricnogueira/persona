cda2cf3c39170ac3e701bb384d83114c1e20f429
fabricnogueira <fabricio221@gmail.com>
2023-11-06 10:34:49 -0300

    Updated routing configuration to utilize Devise for user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root 'home#index'
+  root 'pages#home'
 
-  # Defina a rota para login como sessions#new
-  get 'login', to: 'login#new', as: 'login'
+  devise_for :users, controllers: {
+    registrations_controller: 'users/registrations',
+    sessions_controller: 'users/sessions',
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
+
+  # Defina a rota para login como sessions
+  # get 'login', to: 'login#new', as: 'login'
 end
