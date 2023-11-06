c8cee1357a2a261b26071cd85a370614dc15fbc5
fabricnogueira <fabricio221@gmail.com>
2023-11-06 10:35:06 -0300

    Refactor development environment configuration for better readability

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,10 +8,9 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
-  # Do not eager load code on boot.
   config.eager_load = false
 
-  # Show full error reports.
+
   config.consider_all_requests_local = true
 
   # Enable/disable caching. By default caching is disabled.
