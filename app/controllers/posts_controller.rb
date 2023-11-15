07b7d1b6590df1fc61d1fc8b381bd0a1c24df5c2
fabricnogueira <fabricio221@gmail.com>
2023-11-15 13:47:26 -0300

    Enhance PostsController with new and create actions and adjust post model validations

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,35 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show]
+
   def index
-    @post = Post.all
+    @posts = Post.all
   end
 
   def show
+    # O filtro 'before_action :set_post' já define a variável @post
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post, notice: 'Post criado com sucesso.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_post
     @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:attachment, :content, :user_id)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,3 @@
-# app/models/post.rb
-
 class Post < ApplicationRecord
   belongs_to :user
-  validates :content, presence: true
-  has_one_attached :attachment
 end
