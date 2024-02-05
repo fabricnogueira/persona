0ae665e422b5eb65e23fd1b6ba9e24cff556d76d
fabricnogueira <fabricio221@gmail.com>
2024-02-05 07:47:30 -0300

    Add empty analyzers array to active_storage in development and production configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,6 +39,8 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
+  config.active_storage.analyzers = []
+
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
